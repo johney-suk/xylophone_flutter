c75343fc2003c899121148ea92a58eb96cfd72ee
hsuk <sukhyunil@naver.com>
2022-09-22 14:55:59 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const XylophoneApp());
+}
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
